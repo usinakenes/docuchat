bb92dd1830158605bf0bf88d9c723256f7536a5f
Umut Sina Kenes <usinakenes1907@gmail.com>
2023-10-10 22:41:39 +0200

    stripe stuff

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,19 @@
+
 import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link'
 import { buttonVariants } from './ui/button'
-import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server'
+import { LoginLink, RegisterLink, getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { ArrowRight } from 'lucide-react'
+import UserAccountNav from './UserAccountNav'
 
 const Navbar = () => {
+
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
+
+
+
   return (
     <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
       <MaxWidthWrapper>
@@ -14,7 +22,7 @@ const Navbar = () => {
 
 
           <div className='hidden items-center space-x-4 sm:flex'>
-            <>
+            {!user ? <>
               <Link 
                 href='/pricing' 
                 className={buttonVariants({
@@ -40,7 +48,22 @@ const Navbar = () => {
                 Get started
                 <ArrowRight className='ml-1.5 h-4 w-4' />
               </RegisterLink>
-            </>
+            </> : <>
+              <Link 
+                href='/dashboard' 
+                className={buttonVariants({
+                  variant: 'ghost',
+                  size: 'sm'
+                })}
+              >
+                Dashboard
+              </Link>
+              <UserAccountNav 
+                name={(!user.given_name || !user.family_name) ? 'Your Account' : `${user.given_name} ${user.family_name}`} 
+                email={user.email ?? ''}
+                imageUrl={user.picture ?? ''}
+              />
+            </>}
           </div>
         </div>
       </MaxWidthWrapper>
