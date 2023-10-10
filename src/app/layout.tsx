6b68cae830bcedcd58e578ec45de69d0b425f821
Umut Sina Kenes <usinakenes1907@gmail.com>
2023-10-10 21:57:43 +0200

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
+import { cn } from '@/lib/utils'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import Providers from '@/components/Providers'
+
+import 'react-loading-skeleton/dist/skeleton.css'
+import { Toaster } from '@/components/ui/toaster'
+
+import 'simplebar-react/dist/simplebar.min.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +24,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Providers>
+        <body className={cn(
+          'min-h-screen font-sans antialiased grainy',
+          inter.className
+        )}>
+          <Toaster />
+          <Navbar />
+          {children}
+        </body>
+      </Providers>
     </html>
   )
 }
